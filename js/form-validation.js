62245b8f8a814d15d667061742e2a25229bbe0e4
SerhiiPylyp <sergey.pilipenko1410@gmail.com>
2022-06-06 02:45:38 +0300

    Add validation for form

--- js/form-validation.js
+++ js/form-validation.js
@@ -1,9 +1,18 @@
 const fullName = document.querySelector('#full-name');
+const sendBtn = document.querySelector('#send-btn');
 
 const validateFullName = (e) => {
-    // const value = e.target.value.split('').filter(i => /[A-Za-zА-Яа-яЁё\s_]/.test(i));
-    // fullName.value = value.join('');
     fullName.value = e.target.value.replace(/[^\p{L} _]/gui, '');
 }
 
+const validateForm = (e) => {
+    const email = document.querySelector('#mail');
+    const tell = document.querySelector('#mobile-number');
+    const isValidEmail = !email.value || /^[^ ]+@[^ ]+\.[a-z]{2,3}$/.test(email.value);
+    const operators = ['096', '097', '098', '067', '068','066', '093', '063'];
+    const isValidTell = /[0-9]{10}$/.test(tell.value) && operators.includes(tell.value.substring(0,3));
+    const isValidName = fullName.value.length >= 3 && fullName.value.length <= 30;
+}
+
 fullName.addEventListener('input', validateFullName);
+sendBtn.addEventListener('click', validateForm);
